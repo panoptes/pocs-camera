ace2ceec5ac583d95640b70e987d62d90d1e955b
Wilfred Tyler Gee <wtylergee@gmail.com>
2022-01-25 12:45:43 -1000

    * Start gphoto2 tether.

--- gpio.py
+++ gpio.py
@@ -1,11 +1,16 @@
+import os
+import re
+import shutil
+import subprocess
 import time
 from enum import IntEnum
 from typing import Optional, List
-
-import pigpio
-from fastapi import FastAPI
 from loguru import logger
-from pydantic import BaseSettings, DirectoryPath
+import pigpio
+import requests
+
+from pydantic import BaseSettings, DirectoryPath, AnyHttpUrl
+from fastapi import FastAPI
 
 
 class State(IntEnum):
@@ -17,6 +22,7 @@ class Settings(BaseSettings):
     base_dir: Optional[DirectoryPath]
     pins: List[int] = [17, 18]
     cam_ids: Optional[List[str]]
+    processes: Optional[List]
 
 
 class Exposure(BaseSettings):
@@ -38,6 +44,22 @@ def startup_tasks():
         gpio.set_mode(pin, pigpio.OUTPUT)
 
 
+def start_gphoto_tether(cam_id, sequence_id):
+    gphoto2 = shutil.which('gphoto2')
+    if not gphoto2:  # pragma: no cover
+        raise Exception('gphoto2 is missing, please install or use the endpoint option.')
+
+    home_dir = os.getenv('HOME')
+
+    for port in list_connected_cameras():
+        filename_pattern = f'${home_dir}/images/{cam_id}/{sequence_id}/%Y%m%dT%H%M%S.%C'
+        print(f'Starting gphoto2 tether for {port=} using {filename_pattern=}')
+        command = [gphoto2, '--port', port, '--filename', filename_pattern]
+
+        proc = subprocess.Popen(command)
+        settings.processes.append(proc)
+
+
 @app.post('/take-pic')
 def take_pic(exposure: Exposure):
     """Take a picture by setting GPIO pin high"""
@@ -51,7 +73,42 @@ def take_pic(exposure: Exposure):
         gpio.write(exposure.pin, State.LOW)
 
         if pic_num == exposure.num_exposures:
-            print(f'Reached {exposure.num_exposures=}, stopping')
             break
         else:
             pic_num += 1
+
+
+@app.get('/list-cameras')
+def list_connected_cameras(endpoint: Optional[AnyHttpUrl] = None):
+    """Detect connected cameras.
+
+    Uses gphoto2 to try and detect which cameras are connected. Cameras should
+    be known and placed in config but this is a useful utility.
+
+    Returns:
+        list: A list of the ports with detected cameras.
+    """
+
+    result = ''
+    if endpoint is not None:
+        response = requests.post(endpoint, json=dict(arguments='--auto-detect'))
+        if response.ok:
+            result = response.json()['output']
+    else:
+        gphoto2 = shutil.which('gphoto2')
+        if not gphoto2:  # pragma: no cover
+            raise Exception('gphoto2 is missing, please install or use the endpoint option.')
+        command = [gphoto2, '--auto-detect']
+        result = subprocess.check_output(command).decode('utf-8')
+    lines = result.split('\n')
+
+    ports = []
+
+    for line in lines:
+        camera_match = re.match(r'([\w\d\s_.]{30})\s(usb:\d{3},\d{3})', line)
+        if camera_match:
+            # camera_name = camera_match.group(1).strip()
+            port = camera_match.group(2).strip()
+            ports.append(port)
+
+    return ports
