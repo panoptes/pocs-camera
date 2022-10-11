600e1ffc5b5e5b78fe3c394832fa873e238fa540
Wilfred Tyler Gee <wtylergee@gmail.com>
2022-10-11 13:57:57 -1000

    * Basic cli beginning.

---
+++ cli.py
@@ -0,0 +1,16 @@
+import celery
+import typer
+
+# Celery app.
+celery_app = celery.Celery()
+celery_app.config_from_object('celeryconfig')
+
+# CLI app.
+typer_app = typer.Typer()
+
+
+@typer_app.command()
+def status(command_list):
+    """Get camera status."""
+    task = celery_app.send_task('camera.status')
+    return celery_app.AsyncResult(task.id)
